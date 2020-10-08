cdc8466911385d6bbf455e41e4b5c634e409316a
Patrick M. Jensen <patmjen@dtu.dk>
2020-10-08 22:04:57 +0200

    Only use costum compile config. if not on Windows

--- functions/mex/compile_all.m
+++ functions/mex/compile_all.m
@@ -4,12 +4,19 @@ compileOptions = {
     ['-I' gorphoPath],...
     ['-I' gorphoPath '/cudablockproc'],...
     'NVCC_FLAGS="--expt-relaxed-constexpr"',...
-    '-f', 'nvcc_g++_c++14.xml'
 };
+
+if ~ispc
+    % If we are not on Windows we use the costum g++ config to compile with
+    % C++14. If an older (or newer?) version of MATLAB is used this may
+    % break.
+    compileOptions = [compileOptions, '-f', 'nvcc_g++_c++14.xml'];
+end
 
 mexFiles = dir('gorpho_mex_*.cu');
 numMexFiles = length(mexFiles);
 
+%%
 for i = 1:numMexFiles
     crntFile = fullfile(mexFiles(i).folder, mexFiles(i).name);
     fprintf('Compiling %s...\n', mexFiles(i).name);
